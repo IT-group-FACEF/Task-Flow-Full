519ef8bf02185c47580cca7b2d098111cc3ebcf8
enzosilva44 <110210457+enzosilva44@users.noreply.github.com>
2024-05-29 23:22:18 -0300

    connect database

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,47 @@
+// app.js
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const db = require('./database');
+require('dotenv').config();
 
+const app = express();
+
+// Configuração de CORS para permitir solicitações OPTIONS de qualquer origem
+app.options('*', cors());
+
+// Configuração de CORS para permitir solicitações da origem localhost:3000
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+app.use(express.json()); // Middleware para processar o corpo das requisições
+
+// Definição das rotas de teste de banco de dados
+app.get('/test-db', async (req, res) => {
+  try {
+    await db.query('SELECT NOW()');
+    res.send('Database is connected');
+  } catch (err) {
+    console.error(err);
+    res.status(200).send('Database is not connected');
+  }
+});
+
+// Importação e uso das rotas de usuário e autenticação
 const userRoutes = require('./routes/userRoutes');
 const authRoutes = require('./routes/authRoutes');
 
-app.use(cors()); // Habilita o CORS para todas as rotas
-app.use(express.json()); // Middleware para processar o corpo das requisições
-
-// Configuração das rotas
 app.use('/api/users', userRoutes); // Use as rotas de usuário definidas em userRoutes
 app.use('/api/auth', authRoutes); // Use as rotas de autenticação definidas em authRoutes
+
+// Definição da rota principal
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 let api = axios.create({
-  baseURL: 'http://localhost:8080/api', // Certifique-se de que o baseURL está correto
+  //baseURL: 'http://localhost:8080/api', // Certifique-se de que o baseURL está correto
+  baseURL: 'https://backend-node-task.onrender.com/api',
   timeout: 10000,
 });
 
